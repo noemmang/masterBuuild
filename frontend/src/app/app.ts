82101d276001e2182fc7d3c0e2e49536b63e7ec3
Noe Mmang <nnguemamifumu@gmail.com>
2026-07-30 15:56:01 +0200

    chore: pantalla de mantenimiento (migracion a Azure)

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
